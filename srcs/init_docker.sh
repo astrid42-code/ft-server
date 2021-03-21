fc95d9cabc7e34bedcef309f090dd96131e203b9
asgaulti <asgaulti@student.42.fr>
2021-03-21 17:37:15 +0100

    ft-server2

--- srcs/init_docker.sh
+++ srcs/init_docker.sh
@@ -1,5 +1,7 @@
-#ACCESS NGINX USER
-chown -R www-data:www-data /var/www/*
-chmod -R 755 /var/www/*
+service php7.3-fpm start
+service nginx start
+service mysql start
 
-
+while true;
+        do sleep 10000;
+done
